9288724e3bb137cfde791255beef44838a7c133f
Tom PoLáKoSz <polakosz@freemail.hu>
2020-09-24 13:33:34 +0200

    Implement Login component

---
+++ src/components/Login.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+  Alert,
+  Container,
+  Col,
+  Button,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import WebsiteDescription from "./WebsiteDescription";
+
+const Login = (props) => {
+  const [passwordType, setPasswordType] = useState("password");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const history = useHistory();
+
+  const togglePassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+    } else {
+      setPasswordType("password");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await props.studentService.login(username, password);
+      history.push("/");
+    } catch (e) {
+      if (e.response.status === 403) {
+        setErrorMessage("Invalid email or password!");
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} lg={6}>
+          <p className="h2">Login</p>
+          {0 < errorMessage.length && (
+            <Alert
+              key="searchBarError"
+              variant="danger"
+              className="text-center text-lg-left"
+            >
+              {errorMessage}
+            </Alert>
+          )}
+          <Form>
+            <Form.Label htmlFor="username" srOnly>
+              Username
+            </Form.Label>
+            <InputGroup className="mb-2 mr-sm-2">
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <i class="fas fa-user"></i>
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                id="username"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </InputGroup>
+            <Form.Label htmlFor="password" srOnly>
+              Password
+            </Form.Label>
+            <InputGroup className="mb-2 mr-sm-2">
+              <InputGroup.Prepend>
+                <InputGroup.Text>
+                  <i className="fa fa-lock" />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                id="password"
+                type={passwordType}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputGroup.Prepend
+                className="passwordIconHover"
+                onClick={togglePassword}
+              >
+                <InputGroup.Text>
+                  <i className="fa fa-eye password-icon" />
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+            </InputGroup>
+            <Button
+              variant="primary"
+              type="submit"
+              className="mb-2 mr-sm-2"
+              onClick={handleSubmit}
+            >
+              Login
+            </Button>
+            <p>
+              Don’t have an account yet?
+              <Link to={"/registration"}> Register now!</Link>
+            </p>
+            <p>
+              or <Link to={"/"}>back to home page</Link>.
+            </p>
+          </Form>
+        </Col>
+        <Col xs={12} lg={6} className="text-center">
+          <WebsiteDescription />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
