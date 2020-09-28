8f4bacdf919f218334f65a26e90bb854da828044
Tom PoLáKoSz <polakosz@freemail.hu>
2020-09-28 10:39:35 +0200

    Implement animated navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Header from "./components/Header";
 import Registration from "./components/Registration";
 import StudentService from "./services/StudentService";
 
 function App() {
   const studentService = new StudentService();
   return (
-    <div>
-      <Router>
-        <Route
-          key="registration"
-          path="/registration"
-          render={() => <Registration studentService={studentService} />}
-        ></Route>
-        <Route
-          key="login"
-          path="/login"
-          render={() => <Login studentService={studentService} />}
-        ></Route>
-      </Router>
-    </div>
+    <Fragment>
+      <Container>
+        <Router>
+          <Header />
+          <Route
+            key="registration"
+            path="/registration"
+            render={() => <Registration studentService={studentService} />}
+          ></Route>
+          <Route
+            key="login"
+            path="/login"
+            render={() => <Login studentService={studentService} />}
+          ></Route>
+        </Router>
+      </Container>
+    </Fragment>
   );
 }
 
