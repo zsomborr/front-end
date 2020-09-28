908a50f487fb9c1a376375a7b896ebfc31e05730
Szpoti <gtagigafan@gmail.com>
2020-09-28 11:42:30 +0200

    Minor design changes

--- src/components/QuestionsPage.js
+++ src/components/QuestionsPage.js
@@ -41,25 +41,48 @@ const QuestionsPage = (props) => {
       <h1 className="commonText">NAVBAR</h1>
       <Container>
         <Row>
-          <Col>
+          {/*<Col>
             <h3 className="commonText">Filter</h3>
             <Container>
               <h2 className="commonText">Filters here</h2>
             </Container>
-          </Col>
+          </Col>*/}
           <Col>
             <h3 className="commonText">Search bar</h3>
-            <FormControl id="search" placeholder="Search..." required />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormControl
+              className="input-group mb-3 mr-3"
+              id="search"
+              placeholder="Search..."
+              required
+            />
           </Col>
           <Col>
-            <Button
-              className="commonText"
-              onClick={() => {
-                setIsModalOpen(!isModalOpen);
-              }}
-            >
-              Add new question
-            </Button>
+            <Row>
+              <Col>
+                <Button
+                  className="commonText"
+                  onClick={() => {
+                    setIsModalOpen(!isModalOpen);
+                  }}
+                >
+                  Search
+                </Button>
+              </Col>
+              <Col className="text-right">
+                <Button
+                  className="commonText"
+                  onClick={() => {
+                    setIsModalOpen(!isModalOpen);
+                  }}
+                >
+                  Add new question
+                </Button>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
