bc60242d3b6784fc3e50b1367fe3a00aef362fae
Szpoti <gtagigafan@gmail.com>
2020-09-28 15:25:28 +0200

    Started implementing single question display

--- src/components/SingleQuestionPage.js
+++ src/components/SingleQuestionPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
-const SingleQuestionPage = () => {
-  return <div></div>;
+const SingleQuestionPage = (props) => {
+  const questionsService = props.questionsService;
+  const [questionDetails, setQuestionDetails] = useState();
+  const [answers, setAnswers] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      return await questionsService.getQuestionDetails(props.match.params.id);
+    };
+    const data = getData();
+    setQuestionDetails(data.question);
+    setAnswers(data.answers);
+  }, [questionsService]);
+
+  return (
+    <Container className="page">
+      <Row className="content-container"></Row>
+    </Container>
+  );
 };
 
 export default SingleQuestionPage;
