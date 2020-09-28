4dfbc813960aaa2ac64aef21a3d296541bcd0b1c
Szpoti <gtagigafan@gmail.com>
2020-09-28 16:09:06 +0200

    safe commit

--- src/components/SingleQuestionPage.js
+++ src/components/SingleQuestionPage.js
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 const SingleQuestionPage = (props) => {
   const questionsService = props.questionsService;
-  const [questionDetails, setQuestionDetails] = useState();
+  const [question, setQuestionDetails] = useState();
   const [answers, setAnswers] = useState();
 
   useEffect(() => {
     const getData = async () => {
-      return await questionsService.getQuestionDetails(props.match.params.id);
-    };
-    const data = getData();
-    setQuestionDetails(data.question);
-    setAnswers(data.answers);
-  }, [questionsService]);
+      const data = await questionsService.getQuestionDetails(
+        props.match.params.id
+      );
 
+      setQuestionDetails(data.data.question);
+      setAnswers(data.data.answers);
+    };
+    getData();
+  }, []);
+
+  if (question === undefined) {
+    return null;
+  }
+  console.log("question", question);
+  console.log("answers", answers);
   return (
     <Container className="page">
-      <Row className="content-container"></Row>
+      <Row className="content-container">
+        <Col>
+          <span className="h3">{question.title}</span>
+          <span className="ml-3">by: Jani</span>
+        </Col>
+        <Col>
+          <Image
+            className="img-fluid img-thumbnail rounded-circle border"
+            src={"missing-profile-pic.jpg"}
+            alt="Profile"
+          />
+        </Col>
+      </Row>
     </Container>
   );
 };
