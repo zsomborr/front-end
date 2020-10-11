e7f6eefc5e333878a40b78902c6caef3694825fb
Tom PoLáKoSz <polakosz@freemail.hu>
2020-10-11 11:17:28 +0200

    Merge QuestionPreview and QuestionDisplay to QuestionsPage

--- src/components/QuestionsPage.js
+++ src/components/QuestionsPage.js
@@ -1,18 +1,21 @@
 import React, { useCallback, useEffect, useState } from "react";
 import {
+  Alert,
+  Badge,
+  Button,
   Container,
   Col,
-  Button,
   Form,
   FormControl,
   Row,
 } from "react-bootstrap";
 import MultiSelect from "react-multi-select-component";
+import { Link } from "react-router-dom";
 import NewQuestionModal from "./modals/NewQuestion";
-import QuestionsDisplayer from "./QuestionsDisplayer";
+import ReactTimeAgo from "react-time-ago";
 
 const QuestionsPage = (props) => {
-  const [questions, setQuestions] = useState();
+  const [questions, setQuestions] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [query, setQuery] = useState("");
   const form = React.createRef();
@@ -35,13 +38,13 @@ const QuestionsPage = (props) => {
           });
           return r;
         };
-        const results = await props.questionsService.searchBy(
+        const response = await props.questionsService.searchBy(
           convert(selectedTechnologies)
         );
-        setQuestions(results);
+        setQuestions(response.data);
       } else {
-        const results = await props.questionsService.search(query);
-        setQuestions(results);
+        const response = await props.questionsService.search(query);
+        setQuestions(response.data);
       }
     } catch (e) {}
   };
@@ -49,7 +52,7 @@ const QuestionsPage = (props) => {
   const getAllQuestions = useCallback(async () => {
     try {
       const response = await props.questionsService.getAll();
-      setQuestions(response);
+      setQuestions(response.data);
     } catch (e) {}
   }, [props.questionsService]);
 
@@ -71,7 +74,7 @@ const QuestionsPage = (props) => {
 
   return (
     <Container className="page">
-      <Row className="content-container">
+      <Row className="content-container px-0 pb-0">
         <NewQuestionModal
           isModalOpen={isModalOpen}
           setIsModalOpen={setIsModalOpen}
@@ -115,16 +118,58 @@ const QuestionsPage = (props) => {
               </Col>
               <Col xs={6} lg={3} className="text-right">
                 <Button onClick={() => setIsModalOpen(!isModalOpen)}>
-                  Add new question
+                  Ask question
                 </Button>
               </Col>
             </Form.Row>
           </Form>
-          <Row>
-            <Col>
-              <QuestionsDisplayer questions={questions} />
-            </Col>
-          </Row>
+        </Col>
+        <Col xs={12}>
+          {questions.length === 0 ? (
+            <Alert variant="info">
+              No questions found...
+              <span onClick={() => setIsModalOpen(true)} className="a ml-1">
+                Ask one
+              </span>
+              !
+            </Alert>
+          ) : null}
+          {questions.map((question) => (
+            <Container className="alert alert-primary">
+              <Row>
+                <Col xs={12}>
+                  <Link to={`/question/${question.id}`}>
+                    <h5>{question.title}</h5>
+                  </Link>
+                </Col>
+                <Col xs={12}>
+                  {question.anonym ? (
+                    "Anonymous"
+                  ) : (
+                    <Link to={`/user/${question.userId_}`}>
+                      {question.username}
+                    </Link>
+                  )}{" "}
+                  | <ReactTimeAgo date={question.submissionTime} /> | Votes:{" "}
+                  {question.vote}
+                </Col>
+                <Col xs={12}>
+                  <h6 className="text-truncate">{question.description}</h6>
+                </Col>
+                <Col>
+                  {question.technologyTags.map((technology) => (
+                    <Badge
+                      key={`technology-${technology.technologyTag}`}
+                      variant="primary"
+                      className="badge-pill mr-1"
+                    >
+                      {technology.technologyTag}
+                    </Badge>
+                  ))}
+                </Col>
+              </Row>
+            </Container>
+          ))}
         </Col>
       </Row>
     </Container>
